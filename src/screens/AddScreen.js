26d2058bd13ee2bbf5d50959c50e6c73072dce68
Rasim <rasimmammadoff1@gmail.com>
2020-11-28 17:49:11 +0400

    Arrows added to dropdown menu

--- src/screens/AddScreen.js
+++ src/screens/AddScreen.js
@@ -1,18 +1,21 @@
+import React, { Fragment, useState } from 'react';
+import { TextInput, Text, Button, StyleSheet, View, SafeAreaView } from 'react-native';
+
 import * as yup from 'yup'
 import { Formik } from 'formik'
-
-import React, { Fragment, useState } from 'react';
-import { TextInput, Text, Button, Alert, StyleSheet, SafeAreaView } from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import DropDownPicker from 'react-native-dropdown-picker';
-
+import { currencies } from '../utils/currencies'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 
 const AddScreen = ({ navigation }) => {
+    const [opened, setOpened] = useState()
+
     return (
         <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
             <Formik
-                initialValues={{ amount: '', name: '', billingDate: new Date().toISOString().slice(0, 10),period:'',periodType:'m' ,description: '', method: 'card', currency: 'dollar' }}
+                initialValues={{ amount: '', name: '', billingDate: new Date().toISOString().slice(0, 10), period: '', periodType: 'm', description: '', method: 'card', currency: 'AZN' }}
                 onSubmit={values => console.log(JSON.stringify(values))}
                 validationSchema={yup.object().shape({
                     amount: yup
@@ -54,33 +57,6 @@ const AddScreen = ({ navigation }) => {
                             onBlur={() => setFieldTouched('description')}
                         />
 
-                        <DropDownPicker
-                            items={[
-                                { label: 'Monthly', value: 'm' },
-                                { label: 'Yearly', value: 'y' },
-                                { label: 'Weekly', value: 'w' },
-
-                            ]}
-                            defaultValue={values.periodType}
-                            containerStyle={{ height: 40 }}
-                            style={{ backgroundColor: '#fafafa' }}
-                            itemStyle={{
-                                justifyContent: 'flex-start'
-                            }}
-                            dropDownStyle={{ backgroundColor: '#fafafa' }}
-                            onChangeItem={(item) => setFieldValue('periodType', item.value)}
-                        />
-
-                        <TextInput
-                            value={values.period}
-                            onChangeText={handleChange('period')}
-                            placeholder="Period"
-                            onBlur={() => setFieldTouched('period')}
-                        />
-                        {touched.period && errors.period &&
-                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.period}</Text>
-                        }
-
                         <DatePicker
                             style={{ width: 200 }}
                             mode="date"
@@ -97,10 +73,60 @@ const AddScreen = ({ navigation }) => {
                                     marginLeft: 0
                                 },
                                 dateInput: {
-                                    marginLeft: 36
+                                    marginLeft: 36,
                                 }
                             }}
                             onDateChange={handleChange('billingDate')}
+                        />
+
+
+
+                        <TextInput
+                            value={values.period}
+                            onChangeText={handleChange('period')}
+                            placeholder="Period"
+                            onBlur={() => setFieldTouched('period')}
+                        />
+                        {touched.period && errors.period &&
+                            <Text style={{ fontSize: 10, color: 'red' }}>{errors.period}</Text>
+                        }
+
+                        <DropDownPicker
+                            items={[
+                                { label: 'Monthly', value: 'm' },
+                                { label: 'Yearly', value: 'y' },
+                                { label: 'Weekly', value: 'w' },
+
+                            ]}
+                            defaultValue={values.periodType}
+                            containerStyle={{ height: 40 }}
+                            isVisible={opened == 'Period'}
+                            onOpen={() => setOpened('Period')}
+                            customArrowDown={() => (<Icon name="arrow-drop-down" size={35} />)}
+                            customArrowUp={() => (<Icon name="arrow-drop-up" size={35} />)}
+                            style={{ backgroundColor: '#fafafa' }}
+                            itemStyle={{
+                                justifyContent: 'flex-start'
+                            }}
+                            dropDownStyle={{ backgroundColor: '#fafafa' }}
+                            onChangeItem={(item) => setFieldValue('periodType', item.value)}
+                        />
+
+
+                        <DropDownPicker
+                            items={currencies}
+                            defaultValue={values.currency}
+                            containerStyle={{ height: 40 }}
+                            style={{ backgroundColor: '#fafafa' }}
+                            isVisible={opened == 'Currency'}
+                            customArrowDown={() => (<Icon name="arrow-drop-down" size={35} />)}
+                            customArrowUp={() => (<Icon name="arrow-drop-up" size={35} />)}
+                            onOpen={() => setOpened('Currency')}
+                            itemStyle={{
+                                justifyContent: 'flex-start'
+                            }}
+                            dropDownStyle={{ backgroundColor: '#fafafa' }}
+                            onChangeItem={(item) => setFieldValue('currency', item.value)}
                         />
 
                         <DropDownPicker
@@ -110,6 +136,10 @@ const AddScreen = ({ navigation }) => {
 
                             ]}
                             defaultValue={values.method}
+                            isVisible={opened == 'Method'}
+                            customArrowDown={() => (<Icon name="arrow-drop-down" size={35} />)}
+                            customArrowUp={() => (<Icon name="arrow-drop-up" size={35} />)}
+                            onOpen={() => setOpened('Method')}
                             containerStyle={{ height: 40 }}
                             style={{ backgroundColor: '#fafafa' }}
                             itemStyle={{
@@ -119,24 +149,9 @@ const AddScreen = ({ navigation }) => {
                             onChangeItem={(item) => setFieldValue('method', item.value)}
                         />
 
-                        <DropDownPicker
-                            items={[
-                                { label: 'Dollar', value: 'dollar' },
-                                { label: 'Azn', value: 'azn' },
-
-                            ]}
-                            defaultValue={values.currency}
-                            containerStyle={{ height: 40 }}
-                            style={{ backgroundColor: '#fafafa' }}
-                            itemStyle={{
-                                justifyContent: 'flex-start'
-                            }}
-                            dropDownStyle={{ backgroundColor: '#fafafa' }}
-                            onChangeItem={(item) => setFieldValue('currency', item.value)}
-                        />
 
                         <Button
-                            title='Sign In'
+                            title='Add'
                             disabled={!isValid}
                             onPress={handleSubmit}
                         />
